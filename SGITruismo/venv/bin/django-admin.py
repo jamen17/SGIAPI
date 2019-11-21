5fb77fde389bd94f1b64c33ab0113f6e930a5740
jamodev <jamen17@gmail.com>
2019-11-21 12:54:36 -0500

    Inicio del proyecto

---
+++ SGITruismo/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/jamodev/Documentos/Galeras/TURISMOANDINO/SGIAPI/SGITruismo/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
